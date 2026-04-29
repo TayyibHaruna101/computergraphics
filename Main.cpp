d9bfc9ec4708fe577f3594fa6f4bd613766b869d
TayyibHaruna101 <tayyib.haruna@pau.edu.ng>
2026-04-30 00:08:58 +0100

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,147 +1,64 @@
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
-
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.1f, 0.1f, 1.27f, 1.0f);\n"
-"}\n\0";
-
-
-
-int main()
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+int main(int argc, char* argv[])
 {
-	// Initialize GLFW
-	glfwInit();
+    SDL_Init(SDL_INIT_VIDEO);
 
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    SDL_Window* window = SDL_CreateWindow("SDL3 Test", 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
+    SDL_Surface* surface1 = IMG_Load("C:/Users/reach/Documents/1.jpg");
+    SDL_Texture* texture1 = SDL_CreateTextureFromSurface(renderer, surface1);
+    /*takes two arguments = name of renderer and name of the surface*/
+    SDL_DestroySurface(surface1);
 
-	//Load GLAD so it configures OpenGL
-	gladLoadGL();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+    SDL_Surface* surface2 = IMG_Load("C:/Users/reach/Documents/2.png");
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, surface2);
+    /*takes two arguments = name of renderer and name of the surface*/
+    SDL_DestroySurface(surface2);
+
+    SDL_Surface* surface3 = IMG_Load("C:/Users/reach/Documents/3.jpg");
+    SDL_Texture* texture3 = SDL_CreateTextureFromSurface(renderer, surface3);
+    /*takes two arguments = name of renderer and name of the surface*/
+    SDL_DestroySurface(surface3);
+
+    SDL_Surface* surface4 = IMG_Load("C:/Users/reach/Documents/5.png");
+    SDL_Texture* texture4 = SDL_CreateTextureFromSurface(renderer, surface4);
+    /*takes two arguments = name of renderer and name of the surface*/
+    SDL_DestroySurface(surface4);
 
 
 
-	// Create Vertex Shader Object and get its reference
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Attach Vertex Shader source to the Vertex Shader Object
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
-	glCompileShader(vertexShader);
-
-	// Create Fragment Shader Object and get its reference
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	// Attach Fragment Shader source to the Fragment Shader Object
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
-	glCompileShader(fragmentShader);
-
-	// Create Shader Program Object and get its reference
-	GLuint shaderProgram = glCreateProgram();
-	// Attach the Vertex and Fragment Shaders to the Shader Program
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	// Wrap-up/Link all the shaders together into the Shader Program
-	glLinkProgram(shaderProgram);
-
-	// Delete the now useless Vertex and Fragment Shader objects
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
 
+    SDL_FRect imgrect1 = { 350, 200, 100, 100 };
+    SDL_FRect imgrect2 = { 250, 300, 100, 100 };
+    SDL_FRect imgrect3 = { 450, 300, 100, 100 };
+    SDL_FRect imgrect4 = { 350, 400, 100, 100 };
 
-	// Vertices coordinates
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-	};
+    SDL_FRect centerImage = { 350, 300, 100, 100 };
+    // below center//first two args x and y points,  last two are height and width
 
-	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
-	GLuint VAO, VBO;
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+            if (event.type == SDL_EVENT_KEY_DOWN)
+                if (event.key.key == SDLK_ESCAPE) running = false;
 
-	// Generate the VAO and VBO with only 1 object each
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	// Make the VAO the current Vertex Array Object by binding it
-	glBindVertexArray(VAO);
-
-	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Introduce the vertices into the VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Configure the Vertex Attribute so that OpenGL knows how to read the VBO
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	// Enable the Vertex Attribute so that OpenGL knows to use it
-	glEnableVertexAttribArray(0);
-
-	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+            SDL_RenderClear(renderer);
+            SDL_RenderTexture(renderer, texture2, NULL, &imgrect2);
+            SDL_RenderTexture(renderer, texture1, NULL, &imgrect1);
+            SDL_RenderTexture(renderer, texture3, NULL, &imgrect3);
+            SDL_RenderTexture(renderer, texture4, NULL, &imgrect4);
+            SDL_RenderTexture(renderer, NULL, NULL, &centerImage);//rect in the first. crops the image to where the rectangle is defined . second renders into the rectangle created
+            SDL_RenderPresent(renderer);
+        }
+    }
 
 
 
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Specify the color of the background
-		glClearColor(0.50f, 0.22f, 0.62f, 2.0f);
-		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		glUseProgram(shaderProgram);
-		// Bind the VAO so OpenGL knows to use it
-		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// Swap the back buffer with the front buffer
-		glfwSwapBuffers(window);
-		// Take care of all GLFW events
-		glfwPollEvents();
-	}
-
-
-
-	// Delete all the objects we've created
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
-	return 0;
-}
+    return 0;
+};
